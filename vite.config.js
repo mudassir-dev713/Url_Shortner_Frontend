68e7977dc672b42c7eaf10a14a181e3924786ecc
Mudassir <this.is.mudassir.204@gmail.com>
2025-06-02 23:11:15 +0500

    fixed everything

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import compression from 'vite-plugin-compression';
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react(), tailwindcss()],
 });
